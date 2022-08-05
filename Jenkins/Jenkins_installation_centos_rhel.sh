c72280ac7c5fcdcc5d50a28e39a9aa1a1dfe38f0
Devops_life <98376417+devopslife999@users.noreply.github.com>
2022-08-05 10:32:32 +0530

    Create Jenkins_installation_centos_rhel.sh

---
+++ Jenkins/Jenkins_installation_centos_rhel.sh
@@ -0,0 +1,18 @@
+sudo yum install wget -y && sudo yum install unzip -y && sudo yum install git -y && sudo yum install maven -y &&
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate &&
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key &&
+
+sudo yum install -y epel-release java-11-openjdk-devel &&
+
+sudo yum install jenkins -y &&
+
+sudo systemctl daemon-reload &&
+
+sudo systemctl start jenkins &&
+
+sudo systemctl enable jenkins &&
+
+service jenkins status
