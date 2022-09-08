1976e08393a819bcd63447f32cf35ca7aa6bbaca
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-09-08 16:21:23 +0530

    Create nexus_centos.sh

---
+++ Nexus/nexus_centos.sh
@@ -0,0 +1,6 @@
+yum install wget -y && yum install unzip -y && yum install java -y &&
+
+wget https://download.sonatype.com/nexus/oss/nexus-2.14.4-03-bundle.zip &&
+
+unzip nexus-2.14.4-03-bundle.zip &&
+
