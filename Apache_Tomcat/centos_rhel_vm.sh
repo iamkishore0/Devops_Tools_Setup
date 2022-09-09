386dee96b5bfb1f32f041bb06cb3e997a77cc346
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-09-09 14:25:48 +0530

    Create centos_rhel_vm.sh

---
+++ Apache_Tomcat/centos_rhel_vm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum install wget unzip java -y 
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.82/bin/apache-tomcat-8.5.82.zip--no-check-certificate 
+unzip apache-tomcat-8.5.82.zip
+rm apache-tomcat-8.5.82/conf/tomcat-users.xml
+rm apache-tomcat-8.5.82/webapps/manager/META-INF/context.xml
+cp tomcat-users.xml apache-tomcat-8.5.82/conf/
+cp context.xml apache-tomcat-8.5.82/webapps/manager/META-INF/
+chmod 777 apache-tomcat-8.5.82/bin/startup.sh apache-tomcat-8.5.82/bin/shutdown.sh apache-tomcat-8.5.82/bin/catalina.sh
+./startup.sh
