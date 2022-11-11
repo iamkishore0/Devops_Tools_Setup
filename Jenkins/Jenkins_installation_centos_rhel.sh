482178c4bd6a4977e25b4090acd741022c93dc69
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-11-11 09:53:05 +0530

    Update Jenkins_installation_centos_rhel.sh

--- Jenkins/Jenkins_installation_centos_rhel.sh
+++ Jenkins/Jenkins_installation_centos_rhel.sh
@@ -1,10 +1,9 @@
-sudo yum install wget -y && sudo yum install unzip -y && sudo yum install git -y &&
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate &&
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key &&
-sudo yum install -y epel-release java-11-openjdk-devel &&
-sudo yum install jenkins -y &&
-sudo systemctl daemon-reload &&
-sudo systemctl start jenkins &&
-sudo systemctl enable jenkins &&
+#!/bin/bash
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install fontconfig java-11-openjdk
+yum install jenkins
+service jenkins start
+systemctl enable jenkins
 service jenkins status
