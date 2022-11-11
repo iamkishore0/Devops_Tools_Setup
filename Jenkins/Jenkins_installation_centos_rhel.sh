2164018cedec4d8ef788af1101e7151f4ac42210
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-11-11 10:00:29 +0530

    Update Jenkins_installation_centos_rhel.sh

--- Jenkins/Jenkins_installation_centos_rhel.sh
+++ Jenkins/Jenkins_installation_centos_rhel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install fontconfig java-11-openjdk
