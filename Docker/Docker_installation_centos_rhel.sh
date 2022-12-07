1b55f62f2ae5d65fa3606bafb5f4a87f03cd7b7f
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-12-07 06:33:42 +0530

    Update Docker_installation_centos_rhel.sh

--- Docker/Docker_installation_centos_rhel.sh
+++ Docker/Docker_installation_centos_rhel.sh
@@ -2,7 +2,7 @@ sudo yum install -y yum-utils &&
 sudo yum-config-manager \
    --add-repo \
    https://download.docker.com/linux/centos/docker-ce.repo &&
-sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin &&
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y &&
 sudo service docker start &&
 sudo systemctl enable docker.service &&
 sudo service docker status
