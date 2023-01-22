9e0dbedfd1b5ddbe884c8efe1132f5b1474e8916
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2023-01-22 07:59:08 +0530

    Update centos_rhel_vm.sh

--- Apache_Tomcat/centos_rhel_vm.sh
+++ Apache_Tomcat/centos_rhel_vm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 yum install wget unzip java -y 
-wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.84/bin/apache-tomcat-8.5.84.zip
-unzip apache-tomcat-8.5.84.zip
-rm apache-tomcat-8.5.84/conf/tomcat-users.xml
-rm apache-tomcat-8.5.84/webapps/manager/META-INF/context.xml
-cp tomcat-users.xml apache-tomcat-8.5.84/conf/
-cp context.xml apache-tomcat-8.5.84/webapps/manager/META-INF/
-chmod 777 apache-tomcat-8.5.84/bin/startup.sh apache-tomcat-8.5.84/bin/shutdown.sh apache-tomcat-8.5.84/bin/catalina.sh
-./apache-tomcat-8.5.84/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.zip
+unzip apache-tomcat-8.5.85.zip
+rm apache-tomcat-8.5.85/conf/tomcat-users.xml
+rm apache-tomcat-8.5.85/webapps/manager/META-INF/context.xml
+cp tomcat-users.xml apache-tomcat-8.5.85/conf/
+cp context.xml apache-tomcat-8.5.85/webapps/manager/META-INF/
+chmod 777 apache-tomcat-8.5.85/bin/startup.sh apache-tomcat-8.5.85/bin/shutdown.sh apache-tomcat-8.5.85/bin/catalina.sh
+./apache-tomcat-8.5.85/bin/startup.sh
