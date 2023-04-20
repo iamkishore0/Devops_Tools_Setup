8f7b5aac000984caf089b90605683dafcc1e85da
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2023-04-20 11:15:08 +0530

    Update

--- Jenkins/Jenkins_installation_centos_rhel.sh
+++ Jenkins/Jenkins_installation_centos_rhel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sudo yum install wget -y
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
 yum install fontconfig java-11-openjdk -y
 yum install jenkins -y
 service jenkins start
