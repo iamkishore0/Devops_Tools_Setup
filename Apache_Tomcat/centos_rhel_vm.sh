6f6ac9497b517f5144af574f2261a9706e21908d
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2023-11-15 08:32:53 +0530

    Update centos_rhel_vm.sh

--- Apache_Tomcat/centos_rhel_vm.sh
+++ Apache_Tomcat/centos_rhel_vm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 yum install wget unzip java -y 
-wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.94/bin/apache-tomcat-8.5.94.zip
-unzip apache-tomcat-8.5.94.zip
-rm apache-tomcat-8.5.94/conf/tomcat-users.xml
-rm apache-tomcat-8.5.94/webapps/manager/META-INF/context.xml
-cp tomcat-users.xml apache-tomcat-8.5.94/conf/
-cp context.xml apache-tomcat-8.5.94/webapps/manager/META-INF/
-chmod 777 apache-tomcat-8.5.94/bin/startup.sh apache-tomcat-8.5.94/bin/shutdown.sh apache-tomcat-8.5.94/bin/catalina.sh
-./apache-tomcat-8.5.94/bin/startup.sh
-echo "cd /root/Devops_Tools_Setup/Apache_Tomcat/apache-tomcat-8.5.94/bin && ./startup.sh" >> /root/.bashrc
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.96/bin/apache-tomcat-8.5.96.zip
+unzip apache-tomcat-8.5.96.zip
+rm apache-tomcat-8.5.96/conf/tomcat-users.xml
+rm apache-tomcat-8.5.96/webapps/manager/META-INF/context.xml
+cp tomcat-users.xml apache-tomcat-8.5.96/conf/
+cp context.xml apache-tomcat-8.5.96/webapps/manager/META-INF/
+chmod 777 apache-tomcat-8.5.96/bin/startup.sh apache-tomcat-8.5.96/bin/shutdown.sh apache-tomcat-8.5.96/bin/catalina.sh
+./apache-tomcat-8.5.96/bin/startup.sh
+echo "cd /root/Devops_Tools_Setup/Apache_Tomcat/apache-tomcat-8.5.96/bin && ./startup.sh" >> /root/.bashrc
