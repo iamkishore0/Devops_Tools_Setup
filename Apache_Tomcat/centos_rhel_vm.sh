da35fb947e33be4fd87e51fa7a0cf8cc1105726a
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2024-05-22 11:33:52 +0530

    Update centos_rhel_vm.sh

--- Apache_Tomcat/centos_rhel_vm.sh
+++ Apache_Tomcat/centos_rhel_vm.sh
@@ -7,6 +7,6 @@ rm apache-tomcat-8.5.100/conf/tomcat-users.xml
 rm apache-tomcat-8.5.100/webapps/manager/META-INF/context.xml
 cp tomcat-users.xml apache-tomcat-8.5.100/conf/
 cp context.xml apache-tomcat-8.5.100/webapps/manager/META-INF/
-chmod 777 apache-tomcat-8.5.100/bin/startup.sh apache-tomcat-8.5.100/bin/shutdown.sh apache-tomcat-8.5.99/bin/catalina.sh
+chmod 777 apache-tomcat-8.5.100/bin/startup.sh apache-tomcat-8.5.100/bin/shutdown.sh apache-tomcat-8.5.100/bin/catalina.sh
 ./apache-tomcat-8.5.100/bin/startup.sh
 echo "cd /root/Devops_Tools_Setup/Apache_Tomcat/apache-tomcat-8.5.100/bin && ./startup.sh" >> /root/.bashrc
