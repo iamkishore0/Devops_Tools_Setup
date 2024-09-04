8dd1b088b468be97a80c6d644f5b6e11035685f1
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2024-09-04 10:15:15 +0530

    Create ubuntu.sh

---
+++ Nexus/ubuntu.sh
@@ -0,0 +1,15 @@
+apt-get install wget -y && apt-get install unzip -y && apt-get install java -y &&
+
+wget https://download.sonatype.com/nexus/oss/nexus-2.14.4-03-bundle.zip &&
+
+unzip nexus-2.14.4-03-bundle.zip &&
+
+rm nexus-2.14.4-03/bin/nexus &&
+
+cp nexus nexus-2.14.4-03/bin/ &&
+
+chmod +x nexus-2.14.4-03/bin/nexus &&
+
+bash nexus-2.14.4-03/bin/nexus restart &&
+
+echo "cd /root/Devops_Tools_Setup/Nexus/nexus-2.14.4-03/bin && bash nexus start" >> /root/.bashrc
