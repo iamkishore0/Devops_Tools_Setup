2ec4e621b935a686ac0699ebc95c03055b9d47ba
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2025-09-08 08:17:24 +0530

    Update centos_rhel_vm.sh

--- Apache_Tomcat/centos_rhel_vm.sh
+++ Apache_Tomcat/centos_rhel_vm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-yum install wget unzip java -y 
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.zip
-unzip apache-tomcat-9.0.108.zip
-rm apache-tomcat-9.0.108/conf/tomcat-users.xml
-rm apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
-cp tomcat-users.xml apache-tomcat-9.0.108/conf/
-cp context.xml apache-tomcat-9.0.108/webapps/manager/META-INF/
+sudo yum install wget unzip java -y 
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.zip
+sudo unzip apache-tomcat-9.0.108.zip
+sudo rm apache-tomcat-9.0.108/conf/tomcat-users.xml
+sudo rm apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+sudo cp tomcat-users.xml apache-tomcat-9.0.108/conf/
+sudo cp context.xml apache-tomcat-9.0.108/webapps/manager/META-INF/
 chmod 777 apache-tomcat-9.0.108/bin/startup.sh apache-tomcat-9.0.108/bin/shutdown.sh apache-tomcat-9.0.108/bin/catalina.sh
-./apache-tomcat-9.0.108/bin/startup.sh
-echo "cd /root/Devops_Tools_Setup/Apache_Tomcat/apache-tomcat-9.0.108/bin && ./startup.sh" >> /root/.bashrc
+sudo ./apache-tomcat-9.0.108/bin/startup.sh
+# echo "cd /root/Devops_Tools_Setup/Apache_Tomcat/apache-tomcat-9.0.108/bin && ./startup.sh" >> /root/.bashrc
